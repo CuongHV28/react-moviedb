be82c42ac7f02eb4783fff3df609e38d2b45ecc2
YouKnowThat <vcuonghoang28@gmail.com>
2021-09-04 15:54:57 +0700

    fix typo className instead of classname

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -28,9 +28,9 @@ const MovieInfo = ({ movie }) => (
                 <div className="rating-directors">
                     <div>
                         <h3>RATING</h3>
-                        <div classname="score">{movie.vote_average}</div>
+                        <div className="score">{movie.vote_average}</div>
                     </div>
-                    <div classname="director">
+                    <div className="director">
                         <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
                         {movie.directors.map(director => (
                             <p key={director.credit_id}>{director.name}</p>
